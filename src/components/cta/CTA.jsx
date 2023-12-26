0b8a57ca8d207636c0ae78fd725662bbb727ac50
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2023-12-27 00:18:31 +0530

    Stable Home Page

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,11 +4,13 @@ import './cta.css';
 const CTA = () => (
   <div className="gpt3__cta">
     <div className="gpt3__cta-content">
-      <p>Request Early Access to Get Started</p>
-      <h3>Register Today & start exploring the endless possibilities.</h3>
+      <p>Read The First Research-Paper</p>
+      <h3>Read The First Research-Paper On GalactoMind AGI on Code Explorer !!!</h3>
     </div>
     <div className="gpt3__cta-btn">
-      <button type="button">Get Started</button>
+    <a href="https://explorecode.live/research-paper/galctomind-0.0.1" target="_blank" rel="noopener noreferrer">
+      <button type="button">Read Now</button>
+      </a>
     </div>
   </div>
 );
